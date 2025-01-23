758971105075326d841f54cba77d50f84b2b1715
Abril Palau Pardillos <abrilpalau14@gmail.com>
2025-01-23 09:48:58 +0100

    first commit

---
+++ ej01.php
@@ -0,0 +1,36 @@
+<!--
+Ejercicio01.php - Crea un formulario que permita gesƟonar la canƟdad de refresco o leche que hay en un supermercado. (2 puntos) 
+    a) Se debe mantener el nombre del trabajador que está uƟlizando la aplicación. 
+    b) Se debe poder añadir y quitar leche o refresco seleccionando de una lista
+    c) Se debe controlar que no se pueden quitar mas unidades de las que haya, en ese caso mostrar error.
+-->
+
+<?php //para procesar la inc¡formacion (CONTROLADOR)
+/* 
+when user click on a button (if from POST has been submitted)
+    1. get data from form
+    2. save worker data 
+    3. detect button
+        3.1. to add products
+            3.1.1. evaluate product
+            3.1.2. add quantity to corresponding product
+
+        3.2. to remove products
+            3.2.1. evaluate product
+            3.2.2. check i fquantity is not greater than current one
+            3.2.3. substract from quantity to corresponding product
+*/
+?>
+
+<!-- para interactuar con el usuario (VIEW)-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Supermarket management</title>
+    </head>
+    <body>
+        <h1>Supermarket management</h1>
+<!--tiene que tener 3 bortones -->
+
+    </body>
+</html>
